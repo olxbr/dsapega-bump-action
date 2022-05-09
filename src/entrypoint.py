8fb5f34ccae63013a16ed0d5c16d0f38e7f83eb2
Márcio Eduardo Gomes Barbosa <rgb.marcio@gmail.com>
2022-05-09 16:55:29 -0300

    Add variable handling on the entrypoint

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,5 +1,9 @@
 import argparse
+import os
 import json
+import get_languages_from_repo
+
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 
 def main():
@@ -7,12 +11,20 @@ def main():
     parser.add_argument("-c", "--config", help="Action configs", default="")
     args = parser.parse_args()
     cfg = args.config
-    print("--------------------------------")
-    print(cfg[1:])
-    print("--------------------------------")
-    print(cfg[:1])
-    print("--------------------------------")
-    print(f"O valor de teste é {json.loads(cfg)['test']}")
+
+    configs = json.loads(cfg)
+
+    repo = os.getenv('GITHUB_ACTION_REPOSITORY', 'no-name')
+    default_branch = os.getenv('DEFAULT_BRANCH', 'main')
+
+    with logging_redirect_tqdm():
+        get_languages_from_repo.process(
+            repo=repo,
+            token=configs['token'],
+            default_branch=default_branch,
+            verbose=False,
+            bucket=configs['s3-bucket'],
+        )
 
 
 if __name__ == '__main__':
